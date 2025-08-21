759906e80e4a238e881aa57a8897f6c97533fe79
Mac <mac@Hendriis-MacBook-Pro.local>
2025-08-21 16:13:25 +0700

    rubah dashboard dan role permission

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ApiAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [ApiAuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/ots', [\App\Http\Controllers\OTSController::class, 'apiStore']);
+    Route::get('/ots/{slug}', [\App\Http\Controllers\OTSController::class, 'apiShow']);
 });
